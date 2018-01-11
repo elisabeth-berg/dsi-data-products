017a1e15df47949768d4b5c0dfbee4952428011c
Liz Berg <elisabeth.berg921@gmail.com>
2018-01-10 17:09:15 -0800

    Edit color dictionary

--- sample_site/model.py
+++ sample_site/model.py
@@ -3,10 +3,10 @@ import numpy as np
 class ColorDict():
     def __init__(self):
         self.color_dict = {
-        'red' : ['an apple', 'some chair', 'a water bottle', 'a firetruck'],
-        'green' : ['an apple', 'kale', 'rabbit food', 'an avocado'],
-        'blue' : ['a rainjacket', 'an eye', 'a larkspur', 'a recycle bin'],
-        'yellow' : ['eggnog', 'mustard', 'some sweater', 'the sun']}
+        'red' : ['an apple', 'a sunburn', "Liz's water bottle", 'a firetruck', 'blood'],
+        'green' : ['a Granny Smith apple', 'kale', 'rabbit food', 'an avocado'],
+        'blue' : ['a rainjacket', 'an eye', 'a larkspur', 'a recycle bin', 'this website'],
+        'yellow' : ['eggnog', 'mustard', 'the sun', 'a post-it note']}
 
     def predict(self, color):
         return np.random.choice(self.color_dict[color], 1)
